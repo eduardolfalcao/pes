09bca0c51c2b5be592d30d9505fdbc424c5ae3fd
Eduardo Falcão <eduardo@dca.ufrn.br>
2024-12-12 23:40:42 -0300

    Add bug

--- calc.py
+++ calc.py
@@ -8,7 +8,7 @@ def subtract(a, b):
     return a - b
 
 def multiply(a, b):
-        return np.multiply(a, b)
+        return np.multiply(a, -b)
 
 if __name__ == "__main__":
     print(f"Addition: {add(1, 2)}")
